1fb306f675ee6cd3b0cb63cd439d4060174857c1
ardiakov <adiakov.work@gmail.com>
2017-12-11 17:30:45 +0300

    Тест для случая, когда нет обработки queryBuilder-а

--- tests/FilterManagerTest.php
+++ tests/FilterManagerTest.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\QueryBuilder;
 use PHPUnit\Framework\TestCase;
 use Ruvents\FilterBundle\FilterManager;
 use Ruvents\FilterBundle\tests\Type\FilterTypeTest;
+use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
 use Symfony\Component\Form\FormFactory;
 use Symfony\Component\Form\FormRegistry;
 use Symfony\Component\Form\ResolvedFormTypeFactory;
@@ -27,14 +28,6 @@ class FilterManagerTest extends TestCase
 
     /** @var EntityManager */
     private $entityManager;
-
-    protected function setUp()
-    {
-        $this->container = new TestContainer();
-        $this->formFactory = new FormFactory(new FormRegistry([], (new ResolvedFormTypeFactory())));
-        $this->requestStack = new RequestStack();
-        $this->entityManager = $this->getMockBuilder(EntityManagerInterface::class)->getMock();
-    }
 
     /**
      * @expectedException \LogicException
@@ -61,5 +54,35 @@ class FilterManagerTest extends TestCase
 
         (new FilterManager($this->container, $this->formFactory, $this->requestStack))
             ->apply(FilterTypeTest::class, $qb);
+    }
+
+    public function testApply()
+    {
+        $serviceId = FilterTypeTest::class;
+        $this->container->set($serviceId, new FilterTypeTest());
+
+        $requestStack = new RequestStack();
+        $requestStack->push(new Request());
+
+        $qb = (new QueryBuilder($this->entityManager));
+
+        $filterManager = new FilterManager($this->container, $this->formFactory, $requestStack);
+
+        $filterResult = $filterManager->apply(FilterTypeTest::class, $qb, $options = []);
+
+        $testForm = (new FilterTypeTest())->createForm($this->formFactory, []);
+
+        $this->assertEquals($filterResult->getForm(), $testForm);
+        $this->assertEquals($filterResult->getQueryBuilder(), $qb);
+        $this->assertEquals($filterResult->getOptions(), $options);
+        $this->assertEquals($filterResult->createView(), $testForm->createView());
+    }
+
+    protected function setUp()
+    {
+        $this->container = new TestContainer();
+        $this->formFactory = (new FormFactory(new FormRegistry([new HttpFoundationExtension()], new ResolvedFormTypeFactory())));
+        $this->requestStack = new RequestStack();
+        $this->entityManager = $this->getMockBuilder(EntityManagerInterface::class)->getMock();
     }
 }
