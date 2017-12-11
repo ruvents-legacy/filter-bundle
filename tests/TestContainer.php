7af3f3939aa2da81036ca1630be2a91ca4b1d7f2
ardiakov <adiakov.work@gmail.com>
2017-12-11 15:58:01 +0300

    TestContainer

--- tests/TestContainer.php
+++ tests/TestContainer.php
@@ -1,6 +1,36 @@
 <?php
+namespace Ruvents\FilterBundle\tests;
 
-class TestContainer
+use Psr\Container\ContainerInterface;
+
+class TestContainer implements ContainerInterface
 {
+    private $container = [];
 
+    public function set($name, $data)
+    {
+        $this->container[$name] = $data;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($id)
+    {
+        if (isset($this->container[$id])){
+            return $this->container[$id];
+        }
+
+        throw new \OutOfBoundsException();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function has($id)
+    {
+        return isset($this->container[$id]);
+    }
 }
